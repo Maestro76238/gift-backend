35a7301586c5d858751ff18326ba6ce36154a398
aleksandr <ksalow777@gmail.com>
2025-12-14 00:15:46 +0300

    fix admin route

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 console.log("TEST URL:", process.env.SUPABASE_URL);
-console.log("TEST KEY:", process.env.SUPABASE_ANON_KEY ? "LOADED" : "MISSING");
+console.log("TEST KEY:", process.env.SUPABASE_SERVICE_ROLE_KEY ? "LOADED" : "MISSING");
 console.log("ALL ENVS:", Object.keys(process.env));
 
 
@@ -10,5 +10,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
