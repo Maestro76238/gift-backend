77b7ed40f83d55ec95c11412911348f00bfaf2a9
aleksandr <ksalow777@gmail.com>
2025-12-14 08:47:03 +0300

    tg bot

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -1,14 +1,8 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-console.log("TEST URL:", process.env.SUPABASE_URL);
-console.log("TEST KEY:", process.env.SUPABASE_SERVICE_ROLE_KEY ? "LOADED" : "MISSING");
-console.log("ALL ENVS:", Object.keys(process.env));
-
-
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
+const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
+
+export default supabase;
