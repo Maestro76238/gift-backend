c6a98507b4fdb15857323c524a075a6dfd80308c
aleksandr <ksalow777@gmail.com>
2025-12-16 21:29:48 +0300

    FIX BACK

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,6 @@ app.use(
 app.options("*", cors());
 app.use(express.json());
 
-const checkout = new YooCheckout({
-  shopId: process.env.YOOKASSA_SHOP_ID,
-  secretKey: process.env.YOOKASSA_SECRET_KEY,
 });
 
 // ================= SUPABASE INIT =================
