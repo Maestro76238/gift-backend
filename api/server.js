8de1c6e9cfb18495da015c0bfcbca6dce92d05d3
aleksandr <ksalow777@gmail.com>
2025-12-25 13:14:59 +0300

    ZAEBALO

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 app.use(express.json());
-app.use(express.static(join(__dirname, '../public')));
+app.use(express.static(join(__dirname, 'public')));
 
 console.log("⚡ Бот запущен");
 
